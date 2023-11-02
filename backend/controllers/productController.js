09df03393d0c7bb4d286a25f4f58ced647dbba8f
GyaneshwerJha <keshavk72411@gmail.com>
2023-11-02 22:56:14 +0530

    error handling completed

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,25 +1,27 @@
 const Product = require("../models/productModel")
 const ErrorHandler = require("../utils/errorHandler")
+const catchAsyncError = require("../middlewares/catchAsyncError");
+
 //Create product
-exports.createProduct = async (req, res, next) => {
+exports.createProduct = catchAsyncError(async (req, res, next) => {
     const product = await Product.create(req.body);
 
     res.status(201).json({
         success: true,
         product
     })
-}
+})
 
 // Get all product
-exports.getAllProducts = async (req, res) => {
+exports.getAllProducts = catchAsyncError(async (req, res) => {
     const products = await Product.find();
 
     res.status(200).json({ success: true, products })
 
-}
+})
 
 // Update Product -- Admin Route only
-exports.updateProduct = async (req, res, next) => {
+exports.updateProduct = catchAsyncError(async (req, res, next) => {
     let product = await Product.findById(req.params.id);
     if (!product) {
         return next(new ErrorHandler("Product not found", 404));
@@ -38,11 +40,11 @@ exports.updateProduct = async (req, res, next) => {
         product: product
     });
 
-}
+})
 
 //Delete Product -- Admin Route only
 
-exports.deleteProduct = async (req, res, next) => {
+exports.deleteProduct = catchAsyncError(async (req, res, next) => {
     const product = await Product.findById(req.params.id);
 
     if (!product) {
@@ -57,20 +59,19 @@ exports.deleteProduct = async (req, res, next) => {
         message: "Product Deleted Successfully"
     })
 }
+)
 
 //Get Product Details
 
-exports.getProductDetails = async (req, res, next) => {
+exports.getProductDetails = catchAsyncError(async (req, res, next) => {
     const product = await Product.findById(req.params.id);
 
     if (!product) {
         return next(new ErrorHandler("Product not found", 404));
 
     }
-
-
     res.status(200).json({
         success: true,
         product: product
     })
-}
+})
--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,30 @@ const mongoose = require('mongoose');
 
 const dotenv = require("dotenv");
 //config
-dotenv.config({path:"backend/config/config.env"})
+dotenv.config({ path: "backend/config/config.env" })
 
 //connecting to database
-mongoose.connect('mongodb://127.0.0.1:27017/Ecommerce').then(()=>console.log("DataBase Connected"))
-.catch((e) => console.log(e));
+mongoose.connect('mongodb://127.0.0.1:27017/Ecommerce').then(() => console.log("DataBase Connected"))
+    .catch((e) => console.log(e));
 
-app.listen(process.env.PORT, ()=>{
+// Handling Uncaught Exception
+process.on("uncaughtException", (err) => {
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server due to Uncaught Exception`);
+    process.exit(1);
+});
+const server = app.listen(process.env.PORT, () => {
 
     console.log(`Server is working on http://localhost: ${process.env.PORT}`)
 
 })
+
+//unhandled promise rejection
+process.on("unhandledRejection", (err) => {
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+
+    server.close(() => {
+        process.exit(1);
+    });
+});
